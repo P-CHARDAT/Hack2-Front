73f8a4c8b09f1b6cf2146c0b4b3fa70eaeaeef40
axel <chapellieraxel@gmail.com>
2021-06-28 21:31:34 +0200

    animation 1

--- front/src/components/Accueil.jsx
+++ front/src/components/Accueil.jsx
@@ -1,9 +1,22 @@
 import React from "react";
+import "../styles/Accueil.css";
 import { Link } from "react-router-dom";
 
 function Accueil() {
   return (
     <div>
+      <div className="contenair-animation">
+        <div className="ecranNoir">
+        </div>
+        <div className="traitBlanc1"></div>
+        <div className="traitBlanc3"></div>
+        <div className="carre1"></div>
+        <div className="carre2"></div>
+        <div className="rondNoir"></div>
+        <div className="traitBlanc4"></div>
+        <div className="traitBlanc5"></div>
+      </div>
+
       <Link to="/Login">
         <button>Hello</button>
       </Link>
