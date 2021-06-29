335f977886857e17d950ae8a86e951ee503885e9
Pierre <pierre.chardat@neuf.fr>
2021-06-29 19:16:23 +0200

    Début du css page project

--- front/src/components/Category.jsx
+++ front/src/components/Category.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Header from "./Header/Header";
+import "../styles/Category.css";
 
 function Category({ versAccueil }) {
   return (
