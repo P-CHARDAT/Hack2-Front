2c65dcf90a171d88f16379e37adb807f47f20813
Pierre <pierre.chardat@neuf.fr>
2021-06-29 15:55:48 +0200

    Button Anim accueil

--- front/src/components/Accueil.jsx
+++ front/src/components/Accueil.jsx
@@ -1,27 +1,36 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import Loading from "./Loading/Loading"; import "../styles/Accueil.css";
+import Loading from "./Loading/Loading";
+import "../styles/Accueil.css";
+import Header from "./Header/Header.jsx";
 
 function Accueil() {
-  const [chargement, setChargement] = useState(false)
+  const [chargement, setChargement] = useState(false);
   function load(event) {
     setTimeout(function () {
-      setChargement(true) // => "click"
+      setChargement(true); // => "click"
     }, 2750);
-
   }
   return (
     <div onLoad={load} className={chargement ? "scroll" : "no-scroll"}>
-
       <Loading />
       <div className="background-accueil">
+        <Header />
         <section className="accueil-part1">
           <article className="accueil-quoi">quoi qui</article>
-          <img className="accueil-image1" src={require('../images/Logo_fiverr_blanc.png')} alt="image1" />
+          <img
+            className="accueil-image1"
+            src={require("../images/Logo_fiverr_blanc.png")}
+            alt="image1"
+          />
         </section>
         <section className="accueil-part2">
           <article className="accueil-ou">quoi qui</article>
-          <img className="accueil-image2" src={require('../images/Logo_fiverr_blanc.png')} alt="image1" />
+          <img
+            className="accueil-image2"
+            src={require("../images/Logo_fiverr_blanc.png")}
+            alt="image1"
+          />
         </section>
         <article className="accueil-pourquoi">quoi qui</article>
         <Link to="/Login">
