44fa09778a8037ddde0fc296f5d0d8d1fbbb8c54
Pierre <pierre.chardat@neuf.fr>
2021-06-29 11:08:07 +0200

    Composant Header avec logo importé

--- front/src/components/Accueil.jsx
+++ front/src/components/Accueil.jsx
@@ -2,16 +2,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Loading from "./Loading/Loading";
+import Header from "./Header/Header";
 
 function Accueil() {
   return (
-    <>
+    <div>
       <Loading />
+      <Header />
 
       <Link to="/Login">
         <button>Hello</button>
       </Link>
-    </>
+    </div>
   );
 }
 
--- front/src/components/Header/Header.jsx
+++ front/src/components/Header/Header.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import Logo_fiverr_blanc from "../../images/Logo_fiverr_blanc.png";
 
 function Header() {
   return (
     <section>
-      <img className="logo" alt="logo" src="../images/Logo_fiverr_blanc.png" />
+      <img className="logo" alt="logo" src={Logo_fiverr_blanc} />
     </section>
   );
 }
